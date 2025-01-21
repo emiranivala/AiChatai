3c38dc65200d33e7a046b2c93c8791174c86bcbe
emiranivala <taitufam@gmail.com>
2025-01-21 11:01:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 API_ID = int(environ.get("API_ID", "24210243"))
 API_HASH = environ.get("API_HASH", "509031fb3790b968e489f71d591ebce5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002294535138"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002458919549"))
 ADMINS = int(environ.get("ADMINS", "8050649116"))
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "chatgpthanzobot")
